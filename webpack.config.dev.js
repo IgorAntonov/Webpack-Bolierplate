98c21d860051ded8f00fb2301963f6ebf8fd9e65
Igor Antonov <igorr1496@gmail.com>
2018-04-23 16:35:57 +0200

    add dev and prod configs

---
+++ webpack.config.dev.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = (env, argv) => {
+ 
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/'
+    },
+    mode: 'development',
+    devtool: 'cheap-module-source-map',
+    devServer: {
+      host: 'localhost',
+      port: '3000',
+      contentBase: path.resolve(__dirname, 'public'),
+      watchContentBase: true,
+      publicPath: '/',
+      clientLogLevel: "none",
+      compress: true,
+      historyApiFallback: true,
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [
+                  autoprefixer({
+                    grid: true,
+                    flexbox: 'no-2009'
+                  })
+                ]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 10000
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        favicon: path.resolve(__dirname, 'public/favicon.ico'),
+        inject: true
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  }
+};
